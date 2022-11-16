0d1835a66a1626530a7a250f9b90756aaa3d4e3c
JoaquinCerdan <jjcl4@alu.ua.es>
2022-11-16 01:20:58 +0100

    changes

--- src/router/index.js
+++ src/router/index.js
@@ -7,42 +7,82 @@ const routes = [
   {
     path: '/',
     name: 'post',
-    component: () => import(/* webpackChunkName: "about" */ '../views/PostView.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/PostView.vue'),
+    meta: {
+      title: 'Posts Page'
+    }
+  },
+  {
+    path: '/post/:id',
+    name: 'Individual Post',
+    component: () => import('../views/IndividualPost.vue'),
+    meta: {
+      title: 'Post Page'
+    }
   },
   {
     path: '/project',
     name: 'project',
-    component: () => import(/* webpackChunkName: "about" */ '../views/ProjectView.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/ProjectView.vue'),
+    meta: {
+      title: 'Projects Page'
+    }
+  },
+  {
+    path: '/proyect/:id',
+    name: 'Individual Proyect',
+    component: () => import('../views/IndividualProject.vue'),
+    meta: {
+      title: 'Proyect Page'
+    }
   },
   {
     path: '/contact',
     name: 'contact',
-    component: () => import(/* webpackChunkName: "about" */ '../views/ContactView.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/ContactView.vue'),
+    meta: {
+      title: 'Contact Page'
+    }
   },
   {
     path: '/about',
     name: 'about',
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+    meta: {
+      title: 'About me Page'
+    }
   },
   {
     path: '/admin',
     name: 'adminPost',
-    component: () => import(/* webpackChunkName: "about" */ '../views/AdminPost.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/AdminPost.vue'),
+    meta: {
+      title: 'Admin Post Page'
+    }
   },
   {
     path: '/adminProject',
     name: 'adminProject',
-    component: () => import(/* webpackChunkName: "about" */ '../views/AdminProject.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/AdminProject.vue'),
+    meta: {
+      title: 'Admin Project Page'
+    }
   },
   {
     path: '/adminContact',
     name: 'adminContact',
-    component: () => import(/* webpackChunkName: "about" */ '../views/AdminContact.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/AdminContact.vue'),
+    meta: {
+      title: 'Admin Contact Page'
+    }
   },
   {
     path: '/adminAbout',
     name: 'adminAbout',
-    component: () => import(/* webpackChunkName: "about" */ '../views/AdminAbout.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/AdminAbout.vue'),
+    meta: {
+      title: 'Admin About me Page'
+    }
   }
 ]
 
